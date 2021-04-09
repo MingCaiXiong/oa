8e9cf0051950b00f193ac812f85f54895aec3459
xmc <xmc000@icloud.com>
2021-04-09 14:05:45 +0800

    test 基层员工请假

--- src/test/java/top/xiongmingcai/oa/service/AdmLeaveFormServiceTest.java
+++ src/test/java/top/xiongmingcai/oa/service/AdmLeaveFormServiceTest.java
@@ -9,6 +9,9 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class AdmLeaveFormServiceTest extends TestCase {
+    /**
+     * 基层员工请假72 小时测试用例
+     */
     public void testCreateLeaveForm() {
         AdmLeaveFormService savedForm = new AdmLeaveFormService();
         Calendar calendar = new GregorianCalendar();
@@ -32,6 +35,38 @@ public class AdmLeaveFormServiceTest extends TestCase {
         form.setReason("回家收包谷");
         form.setFormType(1);
         form.setReason("市场部员工请假单 72 小时");
+        form.setCreateTime(new Date());
+
+
+        AdmLeaveForm savedFormLeaveForm = savedForm.createLeaveForm(form);
+        System.out.println("savedFormLeaveForm.getFormId() = " + savedFormLeaveForm.getFormId());
+    }
+    /**
+     * 基层员工请假24 小时测试用例
+     */
+    public void testCreateLeaveForm2() {
+        AdmLeaveFormService savedForm = new AdmLeaveFormService();
+        Calendar calendar = new GregorianCalendar();
+
+        Date endTime = null;
+        Date stateTime = null;
+
+
+        stateTime = new Date();
+        calendar.setTime(stateTime);
+        calendar.setTime(stateTime);
+        calendar.add(Calendar.DATE, 1);
+
+
+        endTime = calendar.getTime();
+
+        AdmLeaveForm form = new AdmLeaveForm();
+        form.setEmployeeId(10L);
+        form.setStartTime(stateTime);
+        form.setEndTime(endTime);
+        form.setReason("回家收包谷");
+        form.setFormType(1);
+        form.setReason("市场部员工请假单 24 小时");
         form.setCreateTime(new Date());
 
 
