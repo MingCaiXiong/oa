3c97d1318004a1370bfe70c82e37e202267973d2
xmc <xmc000@icloud.com>
2021-04-09 13:59:36 +0800

    style variables

--- src/main/java/top/xiongmingcai/oa/service/BusinessConstants.java
+++ src/main/java/top/xiongmingcai/oa/service/BusinessConstants.java
@@ -1,5 +1,5 @@
 package top.xiongmingcai.oa.service;
 
 public class BusinessConstants {
-    public static final int 总经理请假审批时间阀值 = 36;
+    public static final int GeneralManagerLeaveApprovalTimeThreshold = 36;
 }
